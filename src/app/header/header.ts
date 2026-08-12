740335f1c88d7d6535908b6f4cc1840bea9777d8
raphael <raphaeldragoste6@gmail.com>
2026-08-12 23:38:05 +0200

    premier

---
+++ src/app/header/header.ts
@@ -0,0 +1,39 @@
+import { Component, ElementRef, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+})
+export class Header {
+
+
+
+
+  // Variable qui retient si le menu est ouvert ou fermé : is MenuOpen: boolean = false;
+  isMenuOpen: boolean = false;
+
+  constructor(private eRef: ElementRef) {}
+// Alterne l'état (ouvert/fermé)
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  // Ferme le menu quand on clique sur un lien
+  closeMenu(): void {
+    this.isMenuOpen = false;
+  }
+
+  // Écoute tous les clics sur la page
+  @HostListener('document:click', ['$event'])
+  clickout(event: Event) {
+    // Si le clic se fait en dehors du composant header, on ferme le menu
+    if (!this.eRef.nativeElement.contains(event.target)) {
+      this.closeMenu();
+    }
+  }
+
+
+  
+}
---
+++ src/app/principal/principal.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Header } from "../header/header";
+
+@Component({
+  selector: 'app-principal',
+  imports: [Header],
+  templateUrl: './principal.html',
+  styleUrl: './principal.css',
+})
+export class Principal {}
